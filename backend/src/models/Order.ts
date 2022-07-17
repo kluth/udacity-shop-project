e1676095c33575c7ac1869b5dd0db882953696a8
Matthias Kluth <kluthmatthias@gmail.com>
2022-07-17 21:57:18 +0200

    feat(orders): added db logic

--- backend/src/models/Order.ts
+++ backend/src/models/Order.ts
@@ -1,3 +1,5 @@
+import { Pool } from "pg";
+
 interface OrderProps {
     id: number;
     userId: number;
@@ -14,15 +16,40 @@ const Order = (): OrderProps => {
 }
 
 const getAllOrders = async (): Promise<OrderProps[]> => {
-    throw new Error('Not implemented')
+    let pool = new Pool()
+    await pool.connect()
+    let orders: OrderProps[] = []
+    let query = 'SELECT * FROM orders'
+    let result = await pool.query(query)
+    result.rows.forEach(row => {
+        let order: OrderProps = {
+            id: row.id,
+            userId: row.user_id,
+            status: row.status
+        }
+        orders.push(order)
+    })
+    return orders
 }
 
 const getOrder = async (id: number): Promise<OrderProps> => {
-    throw new Error('Not implemented')
+    return await getAllOrders().then(orders => {
+        return orders.find(order => order.id === id)
+    }) || Order()
 }
 
 const createOrder = async (order: OrderProps): Promise<OrderProps> => {
-    throw new Error('Not implemented')
+    let pool = new Pool()
+    await pool.connect()
+    let query = 'INSERT INTO orders (user_id, status) VALUES ($1, $2) RETURNING *'
+    let values = [order.userId, order.status]
+    let result = await pool.query(query, values)
+    let newOrder: OrderProps = {
+        id: result.rows[0].id,
+        userId: result.rows[0].user_id,
+        status: result.rows[0].status
+    }
+    return newOrder
 }
 
 const updateOrder = async (id: number, order: OrderProps): Promise<OrderProps> => {
